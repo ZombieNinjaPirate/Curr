39a0939240e7b99f18403f2703c04b02d22040a6
Black September <black.september@me.com>
2014-03-15 19:42:51 +0100

    Update honsshctrl_NODECLARE.sh
    added license

--- honsshctrl_NODECLARE.sh
+++ honsshctrl_NODECLARE.sh
@@ -5,14 +5,37 @@ shopt -s -o nounset
 
 
 #
-#   Management script for HonSSH
+#   HonSSH management script.
 #
-#   Author:     Black September
 #   Date:       2014, March 1
 #   Version:    1.2.2
 #   Plattform:  n/a
 #
-
+#   Copyright (c) 2014, Black September - Honeypot Development
+# 
+#   All rights reserved.
+# 
+#   Redistribution and use in source and binary forms, with or without modification, are
+#   permitted provided that the following conditions are met:
+#
+#   1. Redistributions of source code must retain the above copyright notice, this list
+#   of conditions and the following disclaimer.
+# 
+#   2. Redistributions in binary form must reproduce the above copyright notice, this
+#   list of conditions and the following disclaimer in the documentation and/or other
+#   materials provided with the distribution.
+# 
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND AN
+#   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+#   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+#   SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+#   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+#   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+#   STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+#   THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
 
 declare -rx Script="${0##*/}"
 declare honssh_tac="honssh.tac"
