ab8b0acc36dee135dd0f51f1ef3f2f562f3508bf
Black September <black.september@me.com>
2014-03-15 16:49:49 +0100

    Create honsshctrl_NODECLARE.sh
    Platform independent script

---
+++ honsshctrl_NODECLARE.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+
+shopt -s -o nounset
+
+
+#
+#   Management script for HonSSH
+#
+#   Author:     Black September
+#   Date:       2014, March 1
+#   Version:    1.2.1
+#	Plattform:	n/a
+#
+
+
+declare -rx Script="${0##*/}"
+declare honssh_tac="honssh.tac"
+declare honssh_log="logs/honssh.log"
+declare honssh_pid="honssh.pid"
+
+
+if [ $# != 1 ]
+then
+    echo 'ERROR: This script requiers one argument'
+    echo "USAGE: $Script HELP"
+    exit 1
+fi
+
+
+function start_honssh()
+{
+    if [ ! -e $honssh_pid ]
+    then
+        echo "Starting honssh in background..."
+        twistd -y $honssh_tac -l $honssh_log --pidfile $honssh_pid
+    else
+        echo "ERROR: There appears to be a PID file already, HonSSH might be running"
+        exit 1
+    fi
+}
+
+
+function stop_honssh()
+{
+    if [ -e $honssh_pid ]
+    then
+        honey_pid="$(cat $honssh_pid)"
+        echo "Attempting to stop HonSSH ($honey_pid)..."
+        kill -15 $honey_pid &>/dev/null
+        if [ $? != 0 ]
+        then
+            echo "ERROR: Unable to stop HonSSH ($honey_pid)"        
+            exit 1
+        else
+            echo "OK: HonSSH has been stopped"
+        fi
+    else
+        echo "ERROR: No PID file was found, HonSSH might not be running."
+        exit 1
+    fi
+}
+
+
+
+function help_honssh()
+{
+cat << _EOF_
+
+    USAGE: $Script [ARGUMENT]
+
+    $Script      START       Start HonSSH
+    $Script      STOP        Stop HonSSH
+    $Script      RESTART     Restart HonSSH
+    $Script      HELP        Show this help
+
+_EOF_
+}
+
+
+if [ $1 = 'START' ]
+then
+    start_honssh
+fi
+
+
+if [ $1 = 'STOP' ]
+then
+    stop_honssh
+fi
+
+
+if [ $1 = 'RESTART' ]
+then
+    stop_honssh
+    sleep 0.5
+    start_honssh
+fi
+
+
+if [ $1 = 'HELP' ]
+then
+    help_honssh
+fi
+
+
+if [[ $1 != 'START' && $1 != 'STOP' && $1 != 'HELP' && $1 != 'RESTART' ]]
+then
+    help_honssh
+fi
+
+
+exit 0
