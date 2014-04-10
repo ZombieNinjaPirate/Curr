be5f7edb698cb21e39c33d916e1a53e7fbd9e7a6
Are Hansen <black.september@me.com>
2014-04-10 22:07:22 +0200

    Update pkg_install.sh

--- pkg_install.sh
+++ pkg_install.sh
@@ -8,7 +8,7 @@
 #   Version:	1.0.0
 #   Plattform:	OpenBSD 5.4 amd64
 #
-#   Copyright (c) 2014, Black September - Honeypot Development
+#   Copyright (c) 2014, Are Hansen - Honeypot Development
 # 
 #   All rights reserved.
 # 
